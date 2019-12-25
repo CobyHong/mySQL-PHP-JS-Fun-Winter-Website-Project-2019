e02267693cce5b2ba29f2ca3e06a0640f2354ac6
Coby Hong <cobyhong@gmail.com>
2019-12-24 19:55:03 -0800

    Update adding_to_tables.sql

--- SQL queries/adding_to_tables.sql
+++ SQL queries/adding_to_tables.sql
@@ -11,4 +11,4 @@ values
 ('Shingeki no Kyojin', 'Attack on Titan', '2009', 'Action', 'Fall', 'https://twist.moe/a/shingeki-no-kyojin/1');
 
 -- making it so that our password to log into database is not encrypted (for js script).
-ALTER USER 'coby'@'localhost' IDENTIFIED WITH mysql_native_password BY '18chca98'
+ALTER USER 'coby'@'localhost' IDENTIFIED WITH mysql_native_password BY '12345'
